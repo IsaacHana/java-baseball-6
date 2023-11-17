35444dd17635dc0f78e0f0cf06ecd262068d34f2
Isaac Han <isaachan233@gmail.com>
2023-11-17 14:59:36 +0900

    feat: 유효성 검사 추가

--- src/main/java/baseball/domain/BaseballNumbers.java
+++ src/main/java/baseball/domain/BaseballNumbers.java
@@ -1,15 +1,35 @@
 package baseball.domain;
 
+import baseball.commonRules.Rules;
+
 import java.util.List;
 
 public class BaseballNumbers {
     private final List<BaseballNumber> numbers;
 
     private BaseballNumbers(List<BaseballNumber> numbers) {
+        validate(numbers);
         this.numbers = numbers;
     }
 
     public static BaseballNumbers valueOf(List<BaseballNumber> numbers) {
         return new BaseballNumbers(numbers);
+    }
+
+    private void validate(List<BaseballNumber> numbers) {
+        validateSize(numbers);
+        validateDuplication(numbers);
+    }
+
+    private void validateSize(List<BaseballNumber> numbers) {
+        if (numbers.size() != Rules.MAX_SIZE.getValue()) {
+            throw new IllegalArgumentException("[ERROR] %d자리 숫자를 입력해주세요.".formatted(Rules.MAX_SIZE.getValue()));
+        }
+    }
+
+    private void validateDuplication(List<BaseballNumber> numbers) {
+        if (numbers.stream().distinct().count() != Rules.MAX_SIZE.getValue()) {
+            throw new IllegalArgumentException("[ERROR] 서로 다른 숫자를 입력해주세요.");
+        }
     }
 }
