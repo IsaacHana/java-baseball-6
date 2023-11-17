6c778ef3548cbb012ad5be16882335a64e4b5c64
Isaac Han <isaachan233@gmail.com>
2023-11-17 13:48:15 +0900

    feat: 기본 컨트롤러 적용

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,8 +1,10 @@
 package baseball;
 
+import baseball.controller.BaseballGame;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
-    	// git Test
+        BaseballGame baseballGame = new BaseballGame();
+        baseballGame.run();
     }
 }
