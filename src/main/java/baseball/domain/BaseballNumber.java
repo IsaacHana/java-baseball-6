d2ce4975efdcb056c3d32eb41851930a639dcc16
Isaac Han <isaachan233@gmail.com>
2023-11-17 14:16:05 +0900

    refactor: 호출시 새로운 객체 대신 미리 만들어놓은 객체 호출하도록 수정

--- src/main/java/baseball/domain/BaseballNumber.java
+++ src/main/java/baseball/domain/BaseballNumber.java
@@ -1,6 +1,18 @@
 package baseball.domain;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.IntStream;
+
 public class BaseballNumber {
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 9;
+    private static final Map<Integer, BaseballNumber> numberCache = new HashMap<>();
+
+    static {
+        IntStream.range(MIN_NUMBER, MAX_NUMBER + 1).forEach((number) -> numberCache.put(number, new BaseballNumber(number)));
+    }
+
     private final int number;
 
     private BaseballNumber(int number) {
@@ -9,11 +21,11 @@ public class BaseballNumber {
 
     public static BaseballNumber valueOf(int number) {
         validateNumber(number);
-        return new BaseballNumber(number);
+        return numberCache.get(number);
     }
 
     private static void validateNumber(int number) {
-        if (number < 1 || number > 9) {
+        if (number < MIN_NUMBER || number > MAX_NUMBER) {
             throw new IllegalArgumentException("[ERROR] 유효하지 않은 숫자입니다. 1~9 사이 숫자를 입력하세요.");
         }
     }
