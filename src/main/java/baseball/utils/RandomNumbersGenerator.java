b4b19e4799e0ae66baca0df6d73ad4ea1d50a9e0
Isaac Han <isaachan233@gmail.com>
2023-11-18 15:06:53 +0900

    feat: 게임 Rules 와 연결

--- src/main/java/baseball/utils/RandomNumbersGenerator.java
+++ src/main/java/baseball/utils/RandomNumbersGenerator.java
@@ -1,5 +1,6 @@
 package baseball.utils;
 
+import baseball.commonRules.Rules;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
@@ -9,8 +10,8 @@ public class RandomNumbersGenerator implements NumbersGenerator {
     @Override
     public List<Integer> generate() {
         List<Integer> numbers = new ArrayList<>();
-        while (numbers.size() < 3) {
-            int randomNumber = Randoms.pickNumberInRange(1, 9);
+        while (numbers.size() < Rules.MAX_SIZE.getValue()) {
+            int randomNumber = Randoms.pickNumberInRange(Rules.MIN_NUMBER.getValue(), Rules.MAX_NUMBER.getValue());
             if (!numbers.contains(randomNumber)) {
                 numbers.add(randomNumber);
             }
