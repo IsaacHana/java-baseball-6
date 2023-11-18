695c906cdaa987b3f81089a34b085d43e8617302
Isaac Han <isaachan233@gmail.com>
2023-11-18 15:21:26 +0900

    refactor: 테스트 로직 수정

--- src/test/java/baseball/utils/RefereeTest.java
+++ src/test/java/baseball/utils/RefereeTest.java
@@ -1,26 +1,30 @@
 package baseball.utils;
 
 import baseball.commonRules.ContinueRules;
+import baseball.domain.BaseballNumber;
 import baseball.domain.BaseballNumbers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
-import java.util.Objects;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class RefereeTest {
-    @DisplayName("1")
+    @DisplayName("동등성 테스트")
     @Test
     void equals() {
-        assertThat(Referee.equals(BaseballNumbers.createByIntegerList(List.of(1, 2, 3)), BaseballNumbers.createByIntegerList(List.of(1, 2, 3)))).isTrue();
+        List<BaseballNumber> computer = BaseballNumbers.createByIntegerList(List.of(1, 2, 3)).getNumbers();
+        List<BaseballNumber> user = BaseballNumbers.createByIntegerList(List.of(1, 2, 3)).getNumbers();
+        List<BaseballNumber> user2 = BaseballNumbers.createByIntegerList(List.of(3, 2, 1)).getNumbers();
+        assertThat(Referee.equals(computer, user)).isTrue();
+        assertThat(Referee.equals(computer, user2)).isFalse();
     }
 
-    @DisplayName("1")
+    @DisplayName("게임 지속 의사 확인 테스트")
     @Test
     void continues() {
-        String userIntention = "1";
+        String userIntention = ContinueRules.CONTINUE.getValue();
         assertThat(Referee.continues(userIntention)).isTrue();
     }
 }
