cfbd35253bc432fef67fb660d04173138c336d26
Isaac Han <isaachan233@gmail.com>
2023-11-19 13:43:43 +0900

    feat: 게임 시작 키에 포함되어있는지와 게임을 계속 진행하는 지에 대한 메소드 추가

--- src/main/java/baseball/commonRules/ContinueRules.java
+++ src/main/java/baseball/commonRules/ContinueRules.java
@@ -1,5 +1,8 @@
 package baseball.commonRules;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 public enum ContinueRules {
     CONTINUE("1"),
     QUIT("2");
@@ -8,6 +11,14 @@ public enum ContinueRules {
 
     ContinueRules(String value) {
         this.value = value;
+    }
+
+    public static boolean containsRule(String userIntention) {
+        return Arrays.stream(values()).anyMatch(continueRules -> Objects.equals(userIntention, continueRules.value));
+    }
+
+    public static boolean continuesGame(String userIntention) {
+        return Objects.equals(userIntention, CONTINUE.getValue());
     }
 
     public String getValue() {
